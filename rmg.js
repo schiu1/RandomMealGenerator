4b9ac8bfd620d8bd8687f7267423f1a79e359110
Stephen <gstephenc@hotmail.com>
2023-06-18 18:01:29 -0400

    fixed TypeError
    why did i use console.log() on fetch?...

--- rmg.js
+++ rmg.js
@@ -1,2 +1,3 @@
-console.log(fetch('https://www.themealdb.com/api/json/v1/1/random.php'))
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
     .then(res => res.json())
+    .then(data => console.log(data))
