f8f566f03fd3024881c44dbc1a92454ed6b9b989
Stephen <gstephenc@hotmail.com>
2023-06-19 17:57:50 -0400

    messing with json objects
    - need to find out how to access json information outside of fetch

--- rmg.js
+++ rmg.js
@@ -1,3 +1,8 @@
 fetch('https://www.themealdb.com/api/json/v1/1/random.php')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        let recipe = data.meals[0];
+        console.log(recipe);
+        console.log(recipe.strMeal);
+        console.log(recipe.strInstructions);
+    })
