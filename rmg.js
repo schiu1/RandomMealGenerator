5506f0026e3782caefd23d138c4b1fa42cbc31d6
Stephen <gstephenc@hotmail.com>
2023-06-21 18:28:29 -0400

    found out about async/await for promises
    - found out how to not rely on putting everything inside then() and be accessible outside of it
    - need to gather all the relevant info from response and display it

--- rmg.js
+++ rmg.js
@@ -1,8 +1,17 @@
-fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+/* fetch('https://www.themealdb.com/api/json/v1/1/random.php')
     .then(res => res.json())
     .then(data => {
         let recipe = data.meals[0];
         console.log(recipe);
         console.log(recipe.strMeal);
         console.log(recipe.strInstructions);
-    })
+    }) */
+
+async function GetRecipe(){
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+    const data =  await response.json().then(data => data.meals[0])
+    console.log(data);
+    console.log(data.strMeal);
+    console.log(data.strInstructions);
+}
+GetRecipe()
